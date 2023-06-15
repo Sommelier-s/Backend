0d2cbeb1dba8e4f5b6470fa75674932d16529f7e
gonzam77 <gonzam77@gmial.com>
2023-06-15 13:35:23 -0300

    feature/dev-Gonza (backend/deleteWine.js) modificado la forma del set isActive

--- src/server/settings/controllers/wine/deleteWine.controller.js
+++ src/server/settings/controllers/wine/deleteWine.controller.js
@@ -13,7 +13,7 @@ const deleteWine = async (req, res) => {
         //Valid if the id is correct
         if (id === "") return res.status(400).json({ status: 400, error: "The id field is empty" });
         if (!esUUID(id)) return res.status(409).json({ status: 409, error: "The id field has no UUID structure" });
-        const product = await Liquor.findByPk(id);
+        const product = await Wine.findByPk(id);
         //Valid if we have a response
         if (!product) return res.status(404).json({ status: 404, message: "Product not found" })
         product.setDataValue('isActive', false);
