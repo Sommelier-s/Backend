f109b709f67f58412db4cf1f518fc2843612f516
gonzam77 <gonzam77@gmial.com>
2023-06-16 19:57:10 -0300

    feature/dev-Gonza (backend/relationships) creada la relaciones

--- src/database/model/relationships.js
+++ src/database/model/relationships.js
@@ -2,10 +2,21 @@ const User = require("./user.model");
 const Wine = require("./wine.model");
 const Liquor = require("./liquor.model");
 const Wine_category = require('./wineCategory.model');
+const Liquor_category = require('./liquorCategory.model');
+
+
+Wine_category.hasMany(Wine);
+Wine.belongsTo(Wine_category);
+Liquor_category.hasMany(Wine);
+Liquor.belongsTo(Liquor_category);
+
 
 module.exports = {
     User,
     Wine,
     Liquor,
     Wine_category,
+    Liquor_category
 }
+
+//feature/dev-Gonza (backend/relationships) creada la relaciones
