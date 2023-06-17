013dc30935c06b74cbb2a569a64676d5e97c07b7
Jesse McCallums <101757754+Jessemccallums@users.noreply.github.com>
2023-06-16 21:04:36 -0400

    Revert "Dev diego"

--- src/server/settings/routes/index.js
+++ src/server/settings/routes/index.js
@@ -3,13 +3,11 @@
 // Third Party Dependencies.
 const { Router } = require("express");
 
+
 // Router Instance.
 const routes = Router();
 
-routes.use("/wine",  require('./WineRoute/wineRoute'));
-routes.use("/auth", require("./UserRoute/userRoute"));
-
-routes.use("/user",(req,res)=>{
+routes.get("/",(req,res)=>{
     res.status(200).json({message:"todo ok"});
 })
 
