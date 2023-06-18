991a4925351648c8569fc95a860de968ef428218
Vicente De Blasi <vicentedb67@gmail.com>
2023-06-18 20:29:45 -0300

    Update deleteLiquor.controller.js

--- src/server/settings/controllers/liquor/deleteLiquor.controller.js
+++ src/server/settings/controllers/liquor/deleteLiquor.controller.js
@@ -21,7 +21,7 @@ const deleteLiquor = async (req, res) => {
         const user = await User.findByPk(userId);
         if (!user) return res.status(404).json({ status: 404, error: "The user does not exist" });
         //Valid if the user is an administrator
-        if (user.isAdmin === false) return res.status(401).json({ status: 401, error: "User is not an administrator" });
+        if (user.is_Admin === false) return res.status(401).json({ status: 401, error: "User is not an administrator" });
         const product = await Liquor.findByPk(id);
         //Valid if we have a response
         if (!product) return res.status(404).json({ status: 404, message: "Product not found" })
