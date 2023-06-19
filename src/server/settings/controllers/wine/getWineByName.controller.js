793e0c05953f27041eca70df137d1ba83526b66d
gonzam77 <gonzam77@gmial.com>
2023-06-19 13:38:17 -0300

    feature/dev-Gonza (backend/putWine.controller) fixed de validation of name

--- src/server/settings/controllers/wine/getWineByName.controller.js
+++ src/server/settings/controllers/wine/getWineByName.controller.js
@@ -4,14 +4,14 @@ const { Op } = require('sequelize');
 const getWineByName = async (req, res) => {
     //wine name
     const { name } = req.query;
-    
+
     try {
         //Valid if the name comes from the query
         if (Object.keys(req.query).length === 0) return res.status(400).json({ status: 400, error: "The name field is required" });
         //Valid if the name is correct
         if (name === "") return res.status(400).json({ status: 400, error: "The id field is empty" });
         const allWine = await Wine.findAll();
-        const response=allWine.filter((wine)=> wine.name.toLowerCase().includes(name.toLowerCase()));
+        const response = allWine.filter((wine) => wine.name.toLowerCase().includes(name.toLowerCase()));
         //Valid if we have a response
         if (!response) return res.status(404).json({ status: 404, message: "Product not found" })
         if (response.length === 0) return res.status(404).json({ status: 404, message: "Product not found" })
--- src/server/settings/controllers/wine/putWine.controller.js
+++ src/server/settings/controllers/wine/putWine.controller.js
@@ -8,7 +8,7 @@ function esUUID(id) {
 const controllerPutWine = async (req, res) => {
     const { userId } = req.query;
     const { id } = req.params;
-    const { name,description, price, stock, picture, isActive} = req.body;
+    const { name, description, price, stock, picture, isActive } = req.body;
 
     try {
         //Valid if the user id comes from the query
@@ -24,10 +24,12 @@ const controllerPutWine = async (req, res) => {
         //Valid if the user is an administrator
         if (user.is_Admin === false) return res.status(401).json({ status: 401, error: "User is not an administrator" });
 
-        const allWines=await Wine.findAll();
-        const products=allWines.filter((wine)=> wine.name.toLowerCase() === name.toLowerCase());
-        if (products.length !== 0) return res.status(400).json({ status: 400, error: "One product already has that name" });
-        //If the product already exists, it returns an error.
+        if (name) {
+            const allWines = await Wine.findAll();
+            const products = allWines.filter((wine) => wine.name.toLowerCase() === name.toLowerCase());
+            if (products.length !== 0) return res.status(400).json({ status: 400, error: "One product already has that name" });
+        }
+        //If the product does exists returns an error.
         const product = await Wine.findByPk(id);
         if (!product) return res.status(400).json({ status: 400, error: "The product does exist" });
 
@@ -45,7 +47,7 @@ const controllerPutWine = async (req, res) => {
         //I return the product data modified
         res.status(201).json({ status: 201, message: "The product was successfully modified", data: product });
     } catch (error) {
-        res.status(500).json({ status: 500, message: error, message })
+        res.status(500).json({ status: 500, message: "Internal server error" })
     }
 }
 
