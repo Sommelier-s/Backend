c0db19727c6b931f2ee5ee8fd402187ed07ba61c
gonzam77 <gonzam77@gmial.com>
2023-06-23 18:11:07 -0300

    feature/dev-Gonza (backend/server/DB) DB deply and server deploy ready

--- src/database/index.js
+++ src/database/index.js
@@ -2,17 +2,20 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 // Environment Variables.
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT, DB_PORT } =
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT, DB_PORT, DB_DEPLOY_RENDER } =
     process.env;
 
 
 
 // Database Local Connection.
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-    port: DB_PORT,
-    logging: false,
-});
+const sequelize = new Sequelize(DB_DEPLOY_RENDER, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    }
+  });
 
 module.exports = sequelize;
