64b2096eb34a79518a2dc81331905ea9eda613fe
gonzam77 <gonzam77@gmial.com>
2023-06-24 20:59:38 -0300

    feature/dev-Gonza (backend/index db) dialect explicit

--- src/database/index.js
+++ src/database/index.js
@@ -21,7 +21,8 @@ if (DEBUG === "true") {
   //Render deploy database
   const sequelize = new Sequelize(process.env.DB_DEPLOY_RENDER, {
     logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    native: false,
+    dialect: DB_DIALECT, // lets Sequelize know we can use pg-native for ~30% more speed
     dialectOptions: {
       ssl: {
         require: true,
--- src/server/settings/controllers/wine/getWine.controller.js
+++ src/server/settings/controllers/wine/getWine.controller.js
@@ -9,7 +9,7 @@ const getAllWines = async (req, res) => {
         if (!response || response.length === 0) return res.status(404).json({ status: 404, message: "Product not found" })
         res.status(200).json({ status: 200, message: "The product was found", data: response })
     } catch (error) {
-        return res.status(500).json({ status: 500, mesagge: "Internal server error" })
+        return res.status(500).json({ status: 500, mesagge: error.message })
     }
 };
 
