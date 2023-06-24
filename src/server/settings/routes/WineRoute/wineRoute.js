e2b421085944ff1a1080b6929ddf92b470637d51
gonzam77 <gonzam77@gmial.com>
2023-06-24 14:01:48 -0300

    feature/dev-Gonza (backend/wine/liquor) delete permanently routes and controllers added

--- src/server/settings/routes/WineRoute/wineRoute.js
+++ src/server/settings/routes/WineRoute/wineRoute.js
@@ -1,20 +1,22 @@
 const { Router } = require('express');
-const getWine = require('../../controllers/wine/getWine.controller')
-const getActiveWine = require('../../controllers/wine/getActiveWines.controller')
-const getWineById = require('../../controllers/wine/getWineById.controller')
-const getWineByName = require('../../controllers/wine/getWineByName.controller')
-const postWine = require('../../controllers/wine/postWine.controller')
-const deleteWine = require('../../controllers/wine/deleteWine.controller')
-const putWine = require('../../controllers/wine/putWine.controller')
+const getWine = require('../../controllers/wine/getWine.controller');
+const getActiveWine = require('../../controllers/wine/getActiveWines.controller');
+const getWineById = require('../../controllers/wine/getWineById.controller');
+const getWineByName = require('../../controllers/wine/getWineByName.controller');
+const postWine = require('../../controllers/wine/postWine.controller');
+const deleteWine = require('../../controllers/wine/deleteWine.controller');
+const putWine = require('../../controllers/wine/putWine.controller');
+const deletePermanentlyWine = require('../../controllers/wine/deletePermanentlyWine.controller');
 
 const router = Router();
 
-router.post('/', postWine );
-router.get('/', getWine );
-router.get('/active', getActiveWine );
-router.get('/:id', getWineById );
-router.get('/name', getWineByName );
-router.put('/:id', putWine );
-router.delete('/:id', deleteWine );
+router.post('/', postWine);
+router.get('/', getWine);
+router.get('/active', getActiveWine);
+router.get('/:id', getWineById);
+router.get('/name', getWineByName);
+router.put('/:id', putWine);
+router.delete('/:id', deleteWine);
+router.delete('/permanently/:id', deletePermanentlyWine);
 
 module.exports = router;
