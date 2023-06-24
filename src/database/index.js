b92bf65dab9822e1f259daf4d05d8b7446c84514
gonzam77 <gonzam77@gmial.com>
2023-06-24 08:05:23 -0300

    feature/dev-Gonza (backend/index DB) i changed DB to local

--- src/database/index.js
+++ src/database/index.js
@@ -8,14 +8,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT, DB_PORT, DB_DEPLOY_R
 
 
 // Database Local Connection.
-const sequelize = new Sequelize(DB_DEPLOY_RENDER, {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
-    }
-  });
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    port: DB_PORT,
+    logging: false,
+});
 
 module.exports = sequelize;
