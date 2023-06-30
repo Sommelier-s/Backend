de1b54ccb0e173a248a220c671f53517dc4c5d2e
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-06-30 15:50:39 -0300

    feature/dev-MiguelCartBuy (backend/controller): He cambiado algunos detalles del controlador de carrito, ahora ya funciona correctamente

--- src/server/settings/controllers/cart/createCart.controller.js
+++ src/server/settings/controllers/cart/createCart.controller.js
@@ -8,29 +8,35 @@ function esUUID(id) {
 const postCart = async (req, res) => {
     //user id
     const { id } = req.query;
-    const { cart, amount } =  req.body;
+    const { cart, amount } = req.body;
+    console.log("id del usuario: ", id);
+    console.log("Amount:", amount);
+    console.log("Cart: ", cart);
 
     try {
+        
         if (Object.keys(req.query).length === 0) return res.status(400).json({ status: 400, error: "The id field is required" });
-        if(!id || id === "" || !esUUID(id)) return res.status(409).json({ status: 409, error: "The id has no UUID structure!" });
-        if(!cart || cart.length === 0 || cart === "") return res.status(409).json({ status: 409, error: "The cart is empty!" });
-        if(!amount || amount === "" || isNaN(amount)) return res.status(409).json({ status: 409, error: "Amount must be a number"});
-
+        if (!id || id === "" || !esUUID(id)) return res.status(409).json({ status: 409, error: "The id has no UUID structure!" });
+        if (!cart || cart.length === 0 || cart === "") return res.status(409).json({ status: 409, error: "The cart is empty!" });
+        if (!amount || amount === "" || isNaN(amount)) return res.status(409).json({ status: 409, error: "Amount must be a number" });
+        
         const findUser = await User.findByPk(id);
-        if (!findUser) return res.status(404).json({ status: 404, error: "The user does not exist!"});
+        if (!findUser) return res.status(404).json({ status: 404, error: "The user does not exist!" });
 
+        
         const findCart = await Cart.findOne({ where: { user_id: id } });
-        if(findCart) return res.status(400).json({ status: 404, error: "The user already has an active cart!"});
+        if (findCart) return res.status(400).json({ status: 404, error: "The user already has an active cart!" });
+
         
         const created = await Cart.create({
             user_id: id,
-            cart: Cart,
+            cart: cart,
             amount: amount,
         })
-        
+
         res.status(200).json({ status: 200, message: "The cart has been created!", data: created });
     } catch (error) {
-        res.status(500).json({ status: 500, error: error.message });  
+        res.status(500).json({ status: 500, error: error.message });
     }
 
 }
