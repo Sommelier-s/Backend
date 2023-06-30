30b9736376d3f777115fa903f8860c9abced2f04
JesseMcCallums <jessemccallums2001@gmail.com>
2023-06-29 23:14:24 -0400

    Fixing few feature
    In this commit I am adding a solution for some bugs

--- src/server/settings/controllers/user/getAllUser.controller.js
+++ src/server/settings/controllers/user/getAllUser.controller.js
@@ -11,7 +11,7 @@ const getAllUser = async (req, res) => {
             }
             return res.json(users);
         }
-    } catch (error) {
+     catch (error) {
         return res.status(500).json({ status: 500, error: error.message })
     }
 }
