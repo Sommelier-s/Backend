076a7dc3d77a42820a1f2d2a660ed74b08d9f35c
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-07-05 22:22:01 -0300

    He agregado la ruta de traer los productos activos

--- src/server/settings/controllers/liquor/getActiveLiquors.controller.js
+++ src/server/settings/controllers/liquor/getActiveLiquors.controller.js
@@ -3,6 +3,7 @@ const { Liquor } = require('../../../../database/model/relationships')
 const getActiveLiquors = async (req, res) => {
     try {
         const products = await Liquor.findAll({ where: { isActive: true } });
+        
         if (!products || products.length === 0) return res.status(404).json({ status: 404, error: "Product wasn't found!" })
         res.status(200).json({ status: 200, message: "The products were found!", data: products })
     } catch (error) {
--- src/server/settings/controllers/wine/getActiveWines.controller.js
+++ src/server/settings/controllers/wine/getActiveWines.controller.js
@@ -2,13 +2,18 @@ const { Wine } = require('../../../../database/model/relationships')
 
 const getActiveWine = async (req, res) => {
     try {
+        //const response = await Wine.findAll();
+
         const response = await Wine.findAll({
             where: {
                 isActive: true
             }
         })
+
+        
         //Valid if we have a response
         if (!response || response.length === 0) return res.status(404).json({ status: 404, message: "Product not found" })
+        
         res.status(200).json({ status: 200, message: "The product was found", data: response })
     } catch (error) {
         return res.status(500).json({ status: 500, mesagge: "Internal server error" })
